1ebe5950c2a4233a9a8aed1bab846cdcbd62cc91
SirSimon04 <simi@engelnetz.de>
2026-01-20 10:23:29 +0100

    Updates

--- srv/catalog-service.ts
+++ srv/catalog-service.ts
@@ -7,17 +7,19 @@ export class CatalogService extends cds.ApplicationService {
 
         this.on(Module.actions.assign, async req => {
             console.log("Assign called", req.user)
+
             const assignmentExists = await SELECT.one.from(ModuleAssignments).where({
                 module_moduleID: req.params.at(-1).moduleID,
                 student_userID: req.user.id
             })
+
             if (assignmentExists) return req.error("Already assigned")
+
             await INSERT.into(ModuleAssignments).entries([{
                 module_moduleID: req.params.at(-1).moduleID,
                 student_userID: req.user.id
             }])
 
-            // const x = await SELECT.one.from(Students).where({userID: req.user.id}).columns(student => { student.userID, student.firstName, student.modules (a => { a.module (m => {m.name})}) });
             req.info("Successfully assigned")
         })
 
